70a99612c7683cc9c5b8f81b796b3ab86de6a915
jade <jadejung95@gmail.com>
2024-02-01 18:54:21 +0900

    get weekday

--- README.md
+++ README.md
@@ -1,1 +1,32 @@
 # calendar
+
+## ✅ What I Learned
+
+### getMonth()
+
+method of a Date object returns the month starting at 0
+
+```
+const firstDayOfMonth = new Date(year, month, 1);
+// day 1 is the first day of the current month
+
+const daysInMonth = new Date(year, month + 1, 0).getDate();
+// jump to the next month, and ask for day 0 to get the last day of the current month
+```
+
+### Date.toLocaleDateString()
+
+Returns a date as a string value appropriate to the host environment's current locale
+
+```
+const dateString = firstDayOfMonth.toLocaleDateString("en-US", {
+    weekday: "long",
+    year: "numeric",
+    month: "numeric",
+    day: "numeric",
+  });
+```
+
+toLocaleString('en-US', options) means to display the options in English
+
+numeric property is used to localize dates, numbers, and currencies, and its value is either true or false.
--- main.js
+++ main.js
@@ -1,3 +1,13 @@
+const weekdays = [
+  "Sunday",
+  "Monday",
+  "Tuesday",
+  "Wednesday",
+  "Thursday",
+  "Friday",
+  "Saturday",
+];
+
 function load() {
   const date = new Date();
 
@@ -5,7 +15,22 @@ function load() {
   const month = date.getMonth();
   const year = date.getFullYear();
 
+  const firstDayOfMonth = new Date(year, month, 1); // 1 is the first day of month
+  const daysInMonth = new Date(year, month + 1, 0).getDate(); // 0 is the last day of month
+
   console.log(day, month + 1, year);
+  console.log("date:", date);
+  console.log("month:", month);
+  console.log("daysInMonth:", daysInMonth);
+  console.log("firstDayOfMonth:", firstDayOfMonth);
+
+  const dateString = firstDayOfMonth.toLocaleDateString("en-US", {
+    weekday: "long",
+    year: "numeric",
+    month: "numeric",
+    day: "numeric",
+  }); // (English, option)
+  console.log("dateString:", dateString);
 }
 
 load();
